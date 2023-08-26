4397053387cbdddba49ec5861a3a0a6f52c08a2f
J <wnsghd1@naver.com>
2023-08-26 21:09:18 +0900

    파일명 변경

---
+++ matrix.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# 1차원
+a1 = np.array([1, 2, 3, 4, 5])
+
+print(a1)
+# type은 ndarray
+print(type(a1))
+# (5개, 차원)
+print(a1.shape)
+# index로 접근 가능
+print(a1[0], a1[1], a1[3])
+a1[1] = 6
+print(a1)
+
+a2 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(a2)
+print(a2.shape)
+print(a2[0, 2], a2[1][2])
+
+a3 = np.array([
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+])
