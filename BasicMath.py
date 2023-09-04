258c6ee81cba43f189f678a2273ea69dc6348ca4
Jun <wnsghd1@naver.com>
2023-09-04 16:38:08 +0900

    Linear Function

--- BasicMath.py
+++ BasicMath.py
@@ -2,3 +2,19 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+def linear_function(x):
+    a = 0.5
+    b = 2
+
+    return a * x + b
+
+print(linear_function(5))
+
+x = np.arange(-10, 10, 0.1)
+y = linear_function(x)
+
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Linear Function')
+plt.show()
